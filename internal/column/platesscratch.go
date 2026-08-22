95a3473407c3a82de34f953dd83f5661903032dd
“jiayun” <jiayun_work@163.com>
2026-08-22 12:09:24 +0800

    fix: copy plate count instead of zeroing scratch

--- internal/column/platesscratch.go
+++ internal/column/platesscratch.go
@@ -3,14 +3,13 @@ package column
 var plateScratch []float64
 
 func sharePlates(v *[]float64) *[]float64 {
-	return v
+	out := make([]float64, len(*v))
+	copy(out, *v)
+	return &out
 }
 
 func fillPlateCount(n float64) float64 {
 	plateScratch = []float64{n}
 	out := sharePlates(&plateScratch)
-	for i := range *out {
-		(*out)[i] = 0
-	}
 	return (*out)[0]
 }
